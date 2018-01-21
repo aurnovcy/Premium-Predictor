d27b177da3d006be3356c271fd5df622f8f5d795
Aurnov Chattopadhyay <aurnovcy@gmail.com>
2018-01-21 11:10:36 -0500

    Add files via upload

---
+++ Website/action_page.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>ViTech Insurance Analytics</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/scrolling-nav.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="vitech.css">
+</head>
+
+<body id="page-top">
+  
+<?php
+$arr = array ('a'=>1,'b'=>2,'c'=>3,'d'=>4,'e'=>5);
+
+echo json_encode($arr);
+?>
+    <!-- Topmost section of page: has the ability to skip down to the area of interest by clicking on the top right words-->
+     <div class="container">
+      <br>
+        <!--Site Logo--> 
+        <a href="https://vitechinc.com">
+          <img src="https://www.vitechinc.com/wordpress/wp-content/uploads/2015/10/Official-Logo-Website.jpg">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    
+        <div class="collapse navbar-collapse" id="navbarResponsive" >
+          <ul class="navbar-nav ml-auto" id = "menu">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#banner">Options</a>&nbsp&nbsp
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#recommendations">Recommendations</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <br>
+ 
+       <header class="bg-primary text-white">
+        <div class="container text-center" id="banner">
+            <h2 id="steps">Your Insurance Options</h2>
+            <p class="lead">Compare coverage and prices side by side</p>
+        </div>
+       </header>
+
+      <br>
+    <!-- Creates the comparison table-->
+      <div class="row">
+          <h2 style="text-align: center; font-size: 40px;" >Platinum, Gold, Silver and Bronze Plans Information</h2>
+      </div>
+      <table width="100%" id = "options">
+        <thead> 
+          <tr id= "steps">
+            <td> Plan Type </td>                  
+            <td> Base Price  </td>
+            <td> AD&D <img title="Accidental Death and Dismemberment" src="http://www.free-icons-download.net/images/blue-info-button-icon-50693.png" alt="information" height="20" width="20">  </td> 
+            <td> Deductible </td>
+            <td> Coverage Amount </td>
+          </tr>
+        </thead>
+        <tbody>
+          <div class="row">
+           <div class="col-md-3"></div>
+           <div class="col-md-3"></div>
+           <div class="col-md-3"></div>
+           <div class="col-md-3"></div>
+         </div>
+           <!--plat-->
+          <div class="row">
+        <tr>
+            <td> <img src="https://blueastral.com/wp-content/uploads/platinum_opt.png" alt="Platinum" style="color: rgb(226, 226, 226)" height="120"; width="120"> </td>
+            <td>$110</td>
+            <td>$50,000</td>
+            <td>$3,000</td>
+            <td>$100,000</td>
+        </tr>
+      </div>
+      <!--gold-->
+      <div class="row">
+        <tr>
+          <td> <img src="http://vignette2.wikia.nocookie.net/criminal-case-grimsborough/images/a/a9/Gold_Medal_%28CCW_Awards%29.png/revision/latest?cb=20160101140102" alt="Gold" style="color: rgb(255, 233, 0)" height="140"; width="100"> </td>
+            <td>$70</td>
+            <td>$30,000</td>
+            <td>$5,000</td>
+            <td>$100,000</td>
+        </tr>
+      </div>
+      <!--silver-->
+      <div class="row">
+        <tr>
+          <td> <img src="https://vignette.wikia.nocookie.net/criminal-case-grimsborough/images/7/7f/Silver_Medal_%28CCW_Awards%29.png/revision/latest?cb=20160101140409" alt="Silver"  style="color: rgb(153, 153, 153)" height="140"; width="100"> </td>
+                    
+            <td>$40</td>
+            <td>$20,000</td>
+            <td>$7,000</td>
+            <td>$100,000</td>
+        </tr>
+      </div>
+      <!--bronze-->
+      <div class="row">
+        <tr>
+          <td> <img src="http://vignette4.wikia.nocookie.net/criminal-case-grimsborough/images/7/71/Bronze_Medal_%28CCW_Awards%29.png/revision/latest?cb=20160101140520" alt="Bronze" style="color: rgb(193, 146, 3);" height="140" width="100"> </td>         
+            <td>$20</td>
+            <td>$10,000</td>
+            <td>$8,000</td>
+            <td>$100,000</td>
+        </tr>
+      </div>
+    </tbody>
+      </table>
+    </div>         
+      
+    <!-- Directs the user to the recommended plan-->
+    <section id="recommendations">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+            <h2 id="steps">Our Insurance Recommendations</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; ViTech Analytics</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+        <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom JavaScript for this theme -->
+    <script src="js/scrolling-nav.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </body>
+</html>
